d9c4b5f1e6d4ecc62c51d09ab55f0984827a656d
FlorianVst <45539755+FlorianVst@users.noreply.github.com>
2022-05-11 18:58:03 +0200

    MainMenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string LevelToLoad;
+
+    public GameObject settingsWindow;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+
+    public void Settings()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        settingsWindow.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
